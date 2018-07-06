54a58962052625d03e994b18df68a0fbf81d9d54
Santiago Rulfi <santiago.rulfi@wunderman.com>
2018-07-06 14:45:55 -0300

    remove auth from persistor

--- src/config/ReduxPersist.js
+++ src/config/ReduxPersist.js
@@ -8,7 +8,7 @@ const REDUX_PERSIST = {
     key: 'primary',
     storage: AsyncStorage,
     // Reducer keys that you do NOT want stored to persistence here.
-    blacklist: ['nav', 'auth'],
+    blacklist: ['nav'],
     // Optionally, just specify the keys you DO want stored to persistence.
     // An empty array means 'don't store any reducers'.
     // whitelist: [],
